b6f68c22dc37e08ccab7e551aa70027b1348cbc4
penguinscontrol <penguinscontrol@gmail.com>
2013-12-09 03:45:37 -0500

    irrthresh full simplify

--- Sensitivity/irrthresh_soma_axon.m
+++ Sensitivity/irrthresh_soma_axon.m
@@ -24,6 +24,8 @@ gridy = flipud(gridy);
 normIrr=max(max(irrad));
 w=mean(diff(posP));
 exprvalue = linspace(1e-2,1e-2,N_pixel^2);
+
+chr2locs =@(nseg) [1:nseg]./nseg-1./(2.*nseg);
 %% One cell per pixel
  for k=1:N_pixel.^2
         h(k)=rectangle('Position',[gridx(k) gridy(k) w,w],'EdgeColor','b');
@@ -32,51 +34,35 @@ exprvalue = linspace(1e-2,1e-2,N_pixel^2);
         posC(k,2)=gridy(k)+w./2;
         
         set(h(k),'FaceColor',irrad(k)/normIrr*[1 1 1]);
+        axonL=gridmax-diam-90-posC(k,1);
+        nseg = ceil(axonL/500);
+        irrmags=[irrad(k) 0 [0 0], zeros(1, nseg)];
+        exprlevs=[exprvalue(k) 0 [0 0], zeros(1,nseg)];
+        chr2locs=[.5 .5 chr2locs(2) chr2locs(nseg)];
+        tot_nseg = length(irrmags);
+        
+        dlmwrite('matlab_irrmag_out',irrmags,' ');
+        dlmwrite('matlab_chr2locs_out',chr2locs,' ');
+        dlmwrite('matlab_expr_out',exprlevs,' ');
+        nrncommand = ['C:\nrn73w64\bin64\nrniv.exe -nobanner -c "mat_nseg='...
+            sprintf('%f',tot_nseg) '" locals_pop.hoc -c quit()'];
+        dos(nrncommand);
+        fprintf('Irradiance was %f \n', irrmags(1));
+        fprintf('Expression was %f',exprlevs(1));
+        cells=importNeuron();
+         if (find(cells.vsoma(15:end)>0))
+             if (length(find(cells.vsoma(15:end)>0))>75)
+                set(somas(k),'FaceColor','g')
+             else
+                set(somas(k),'FaceColor','r')
+             end
+         else
+            set(somas(k),'FaceColor',[.9 .9 .5])
+         end 
+           irrmags = [];
+    chr2locs = []; 
  end
-N_cell=N_pixel^2;
 
 axis([gridmin gridmax gridmin gridmax]);
 
-for n=1:N_cell
-    %soma
-    [irrSoma, exprSoma,locSoma]=findirrad(diam,1,posC(n,:),irrad,exprvalue,posP,N_pixel);
-    %inital segment
-    [irrIN, exprIN,locIN]=findirrad(diam,1,[posC(n,1)+diam,posC(n,2)],irrad,exprvalue,posP,N_pixel);
-    %Thin Segment
-    [irrThin,exprThin,locThin]=findirrad(60,2,[posC(n,1)+diam+30,posC(n,2)],irrad,exprvalue,posP,N_pixel);
-    %Axon Segment
-    axonL=gridmax-diam-90-posC(n,1);
-    nseg = ceil(axonL/500);
-    if axonL > 0
-        [irrmags, chr2locs]=findirrad(axonL,nseg,[posC(n,1)+90+diam,posC(n,2)],irrad,exprvalue,posP,N_pixel);
-    else
-        irrmags = 0;
-        chr2locs = 0.5;
-        nseg = 1;
-    end
-    
-    irrmags = [irrSoma irrIN irrThin irrmags];
-    chr2locs = [locSoma locIN locThin chr2locs];
-    exprlevs = [exprvalue(n) zeros(1,length(irrIN)) zeros(1,length(irrThin)) zeros(1,length(irrmags))];
-    tot_nseg = length(irrmags);
-    
-    dlmwrite('matlab_irrmag_out',irrmags,' ');
-    dlmwrite('matlab_chr2locs_out',chr2locs,' ');
-    dlmwrite('matlab_expr_out',exprlevs,' ');
-    nrncommand = ['C:\nrn73\bin\nrniv.exe -nobanner -c "mat_nseg='...
-        sprintf('%f',tot_nseg)...
-        '" locals_pop.hoc -c quit()'];
-    dos(nrncommand);
-    fprintf('Irradiance was %f \n', irrmags(1));
-    fprintf('Expression was %f',exprlevs(1));
-    retina(n).cells=importNeuron();
-         if (find(retina(n).cells.vsoma(10:end)>0))
-             set(somas(n),'FaceColor','r')
-         else
-            set(somas(n),'FaceColor',[.9 .9 .5])
-         end
-    retina(n).expr = exprlevs(1);
-    
-    irrmags = [];
-    chr2locs = [];
-end
+
